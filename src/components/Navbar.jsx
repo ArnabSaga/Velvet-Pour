d8267795a8a9e78b8c7605cb4a8cd093c9947ce7
ArnabSaga <arnabdey15091@gmail.com>
2025-07-13 01:04:44 +0600

    Fixed issue in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,40 @@
-import React from 'react'
-import {navLinks} from "../../constants/index.js";
-import {useGSAP} from "@gsap/react";
+import { navLinks } from "../../constants/index.js";
+import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
 const Navbar = () => {
     useGSAP(() => {
         const navTween = gsap.timeline({
             scrollTrigger: {
-                trigger: 'nav',
-                start: 'bottom top'
+                trigger: '.navbar',
+                start: 'bottom top ',
+                toggleActions: 'play none none reverse',
             }
         });
 
-        navTween.fromTo('nav', {
-            backgroundColor: 'transparent'
-        }, {
-            backgroundColor: '#00000050',
-            backgroundFilter: 'blur(10px)',
-            duration: 1,
-            ease: 'power1.inOut',
-        });
-    })
+        navTween.fromTo('.navbar',
+            { backgroundColor: 'transparent' },
+            {
+                backgroundColor: '#00000050',
+                backdropFilter: 'blur(10px)',
+                duration: 1,
+                ease: 'power1.inOut'
+            }
+        );
+    }, []);
 
     return (
-        <nav>
-            <div>
+        <nav className="navbar fixed top-0 w-full z-50">
+            <div className="flex items-center justify-between px-4 py-2">
                 <a href="#home" className="flex items-center gap-2">
                     <img
                         src="/images/logo.png"
                         alt="Logo"
+                        className="h-8"
                     />
                     <p>Velvet Pour</p>
                 </a>
-
-                <ul>
+                <ul className="flex gap-4">
                     {navLinks.map((link) => (
                         <li key={link.id}>
                             <a href={`#${link.id}`}>{link.title}</a>
@@ -44,4 +45,5 @@ const Navbar = () => {
         </nav>
     )
 }
-export default Navbar
+
+export default Navbar;
